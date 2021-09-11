81f8daabab03fa79a434b415023899fd79be09ba
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-09-11 09:34:53 +0900

    first commit

---
+++ front/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <v-app>
+      <router-view />
+      <v-app-bar></v-app-bar>
+      <v-main>Hello World</v-main>
+  </v-app>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
