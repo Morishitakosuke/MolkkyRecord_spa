7607b01e0ba0232a2be0e5f3224c75acb92e01cb
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-09-12 22:53:03 +0900

    add: deviseとrailsの日本語化追加

--- api/config/application.rb
+++ api/config/application.rb
@@ -34,6 +34,9 @@ module App
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    # 日本語化
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.test_framework :rspec, 
             view_specs: false, 
