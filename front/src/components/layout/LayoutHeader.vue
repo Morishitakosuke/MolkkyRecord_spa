3befa6f15cd28893d1766e376719e91daebbb0e9
Kosuke Morishita <k.morishita.1028@gmail.com>
2021-09-13 23:35:02 +0900

    layout: ヘッダーのレイアウト作成

---
+++ front/src/components/layout/LayoutHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-container>
+      <header>
+      <v-app-bar
+        app
+        dark
+      >
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-toolbar-title>MolkkyRecord</v-toolbar-title>
+      </v-app-bar>
+    </header>
+    </v-container>
+  </v-app>
+</template>
